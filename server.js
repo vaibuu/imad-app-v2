e79bff125f0681b78c8b679f4af0f926f3bced44
vaibuu <vaibhav.14bcs2071@abes.ac.in>
2017-02-18 13:40:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article_one', function (req, res) {
+    res.send('Article one request and will be Served here');
+});
+
+app.get('/article_two', function (req, res) {
+    res.send('Article two request and will be Served here');
+});
+
+app.get('/article_three', function (req, res) {
+    res.send('Article three request and will be Served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
